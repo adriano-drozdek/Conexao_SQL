cdf64f4732380953108b54e294ea2ffdfea44ef7
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-08-08 22:12:01 -0300

    Mostrando na tela o Id do novo vendedor cadastrado

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -22,7 +24,8 @@ public class Program {
 					"INSERT INTO seller" 
 				  + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 				  + "VALUES " 
-				  + "(?, ?, ?, ?, ?)");
+				  + "(?, ?, ?, ?, ?)",
+				  Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
@@ -30,7 +33,18 @@ public class Program {
 			st.setInt(5, 4);
 
 			int rowsAffected = st.executeUpdate(); 
-			System.out.println("Pronto! Linhas afetadas: " + rowsAffected);
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Pronto! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("Nenhuma linha afetada! ");
+			}
+			
 		}
 
 		catch (SQLException e) {
