5d2d971732635fe1629e589b82bcb087403ac6cb
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-08-14 19:28:38 -0300

    Acréscimo de 200.0 reais para os vendedores do departamento 2

--- src/application/Atualizar.java
+++ src/application/Atualizar.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import db.DB;
 
@@ -13,6 +14,25 @@ public class Atualizar {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+				"UPDATE seller "
+			+ "SET BaseSalary = BaseSalary + ? "
+			+ "WHERE "
+			+ "(DepartmentId = ?)");
+		st.setDouble(1, 200.0);
+		st.setInt(2, 2);
+		
+		int rowsAffected = st.executeUpdate();
+		
+		System.out.println("Pronto, linhas afetadas: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
 	}
 
